dce3541068bb0635ce3c42be1baf9c84b5ededcb
KevalW <keval.waghate@gmail.com>
2022-07-08 19:53:59 +0530

    I changed signup button also created forgot pasword option.It has toast msg

--- app/src/main/java/com/example/noticeboard_doubts/MainActivity.kt
+++ app/src/main/java/com/example/noticeboard_doubts/MainActivity.kt
@@ -3,21 +3,27 @@ package com.example.noticeboard_doubts
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnSignUp: Button
+    private lateinit var signupTxt: TextView
+    private lateinit var forgotTxt: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnSignUp = findViewById(R.id.signUp)
+        signupTxt = findViewById(R.id.signUp)
+        forgotTxt = findViewById(R.id.frgtpassword)
 
-        btnSignUp.setOnClickListener{
+        signupTxt.setOnClickListener{
             val intent = Intent(this,signupPage::class.java)
             startActivity(intent)
+        }
+        forgotTxt.setOnClickListener{
+            Toast.makeText(this@MainActivity,"will update soon", Toast.LENGTH_SHORT).show()
         }
     }
 }
