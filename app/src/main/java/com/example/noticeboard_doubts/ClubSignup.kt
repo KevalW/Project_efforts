ffe70b270f423c3faced1231c90b98b2ca44054c
viraj2688 <113048120+viraj2688@users.noreply.github.com>
2022-09-18 15:06:00 +0530

    added

---
+++ app/src/main/java/com/example/noticeboard_doubts/ClubSignup.kt
@@ -0,0 +1,11 @@
+package com.example.noticeboard_doubts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClubSignup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_club_signup)
+    }
+}
